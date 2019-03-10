f63a5727868b451f3f11c18d7ab8b4d82e50799c
Josh Senay <josh.d.senay@gmail.com>
2019-03-10 16:28:52 -0700

    Exercise 13

--- ex13.js
+++ ex13.js
@@ -1,5 +1,8 @@
 // Exercise 13: Creating a JS Module
 
+// A module is just a way to add a feature, or capability, to your own code. 
+// Usually a module is code someone else wrote that you want to use, like the ' fs' module.
+
 const geometry = require('./geometry');
 
 let area51 = geometry.area(2.8);
