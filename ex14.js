42c977861e93b6fe4793ac18935bec5d5c327e91
Josh Senay <josh.d.senay@gmail.com>
2019-03-10 17:12:30 -0700

    Exercise 14

---
+++ ex14.js
@@ -0,0 +1,22 @@
+// Exercise 14: True and False Tests
+
+console.log(5 > 10 && 7 > 9)
+
+console.log(3 > '5')
+
+console.log(21 === '21')
+
+console.log(78 <= 1023 && 45 > 23 || 34 < 23)
+
+console.log(7 >= 3 && 94 == 23 && 45 < 100 || 89 == 89)
+
+// Also try adding math to your logic practice:
+
+console.log(6 + 34 < 2 * 20 && 78 - 23 > 100 - 30)
+
+console.log(2 + 3 * 67 <= 789)
+
+// Also try adding some of these to variables and see if you can work it out:
+
+let x = 45;
+console.log(5 + x < x && x * 20 > 34 * 2)
