0cb369f27bfbbc1dfe321ad0f39f24c6cd0eb264
Josh Senay <josh.d.senay@gmail.com>
2019-03-10 17:15:19 -0700

    Exercise 14 Notes

---
+++ ex14 - Companion Notes.js
@@ -0,0 +1,95 @@
+// Section 2: Logic and Data
+
+// Exercise 14: True and False Tests
+
+// Part 1: Comparison Operators
+
+/* Equal
+
+   In JavaScript you can test the equality of two varibles by using "==" (Double Equals)
+   or "===" (Triple Equals).
+
+   Think of == as "could be equal"
+   Think of === as "definitely equal"
+
+   For Example:
+
+   1 == '1'
+   True
+
+   1 === '1'
+   False
+
+   '' == 0 
+   True
+
+   '' === 0 
+   False
+
+   When using == JS will let you compare things that are seemingly different like integer 1
+   and a string that contains '1'. 
+
+   When using === JS will hold you to strict requirements that both sides must be absolutely
+   identical in order to be true.
+*/
+
+/* Not Equal
+
+    You can test whether two things are not equal by using "!=" and "!==". 
+
+    Think of != as "could or could not be equal"
+    Think of !== as "definitely not equal"
+
+    For Example:
+
+    1 != '1'
+    False
+
+    1 !== '1'
+    True
+
+    '' != 0
+    False
+
+    '' !== 0
+    True
+*/
+
+/* Greater, Less than, Equal
+
+    You can test whether things are greater than, less than, or even greater than or equal to
+    or less than or equal to using the following comparison operators:
+
+    Think of > as "Greater than"
+    Think of < as "Less than"
+    Think of >= as "Greater than or equal to"
+    Think of <= as "Less than or equal to"
+
+// Part 2: Logic Operators
+
+/*  Logic operators take the result of two or more tests as in (x != y) and enable you to 
+    compare them. 
+
+    The "&&" operator takes care of 'and' comparisons. 
+
+    For Example:
+
+    q == 2 && k == 9 
+
+    ^ This can be read as: "is q equal to 2 and is k equal to 9?" If both sides of the operator
+    are true, then this will result in a true statement.
+
+    The "||" operator takes care of 'or' comparisons. 
+
+    For Example:
+
+    m == 2 || r == 5
+
+    ^ This can be read as: "is m equal to 2 or is r equal to 5?" 
+
+    The "!" takes care of 'not' comparisons. This operator will invert the truth of anything.
+
+    There is a comprehensive guide for operators at the following website:
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+*/
